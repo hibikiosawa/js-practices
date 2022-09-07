ea6948559be9da739682d629724fd440f349dfdc
osawa <hibiki2_627@yahoo.co.jp>
2022-09-07 16:59:38 +0900

    luxonを導入、変更

--- 02.calendar/calendar.js
+++ 02.calendar/calendar.js
@@ -1,27 +1,29 @@
 var minimist = require('minimist');
+var { DateTime } = require('luxon');
 
-var today_date = new Date()
-
-console.log(today_date)
+var today_date = DateTime.local();
 
 var argv = minimist(process.argv.slice(2), {
   string: ['output'],
   default: {
-    m: today_date.getMonth() + 1,
-    y: today_date.getFullYear(),
+    m: today_date.month,
+    y: today_date.year
   }
 });
 
-let date_of_day = new Date(argv.y,argv.m,0) 
-let last_day = date_of_day.getDate();
-let first_day_of_week = date_of_day.getDay();
+let date_of_day = DateTime.fromObject({
+    year: argv.y,
+    month: argv.m
+});
+
+let last_day = date_of_day.daysInMonth;
+
+let first_day_of_week = date_of_day.day;
+
+console.log(first_day_of_week)
 
 console.log ('    ',argv.m,'月',argv.y,'年')
 console.log ( ' 日 月 火 水 木 金 土')
-
-var count = 1;
-count += first_day_of_week;
-
 
 var beginning_of_month = first_day_of_week;
 
@@ -29,6 +31,9 @@ while (beginning_of_month > 0){
   process.stdout.write(''.padStart(3," "))
   beginning_of_month -= 1
 }
+
+var count = 1;
+count += first_day_of_week;
 
 while (count <= last_day + first_day_of_week){
   process.stdout.write(String(count - first_day_of_week).padStart(3," "))
