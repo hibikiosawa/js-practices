014be9f6ca39bed3fe9ba4df7996b7b2ef17eb2f
osawa <hibiki2_627@yahoo.co.jp>
2022-09-13 15:04:30 +0900

    クラスを追加

--- 04.class/class.js
+++ 04.class/class.js
@@ -1,22 +1,26 @@
-process.stdin.resume();
-process.stdin.setEncoding('utf8');
-var input_string = '';
-text = "";
-
-process.stdin.on('data', function(chunk) {
-    input_string += chunk;
-});
-
-valGlobal1 = ""
-process.stdin.on('end', function() {
-    const lines = input_string.split("\n");
-    lines.forEach(i => console.log(i),);
-    valGlobal1 = lines;
-});
-
 var fs = require("fs");
 
-fs.writeFile('out.txt', text, (err, data) => {
-    if(err) console.log(err);
-    else console.log('保存しました');
-  });
+class Memo {
+  createMemo () {
+    process.stdin.resume();
+    process.stdin.setEncoding('utf8');
+    var input_string = '';
+
+    process.stdin.on('data', function(chunk) {
+      input_string += chunk;
+    });
+
+    process.stdin.on('end', function() {
+      const lines = input_string.split("\n");
+      lines.forEach(i => console.log(i));
+    });
+    
+    fs.writeFile('out.txt', lines, (err, data) => {
+      if(err) console.log(err);
+      else console.log('保存しました');
+    });
+  }
+}
+
+const memo = new Memo
+memo.createMemo()
