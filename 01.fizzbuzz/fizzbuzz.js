7783b0d71a512137cbcce41c29956ffa5c954979
hibikiosawa <105911942+hibikiosawa@users.noreply.github.com>
2022-09-16 16:29:45 +0900

    forEachに変更

--- 01.fizzbuzz/fizzbuzz.js
+++ 01.fizzbuzz/fizzbuzz.js
@@ -1,13 +1,12 @@
-let num = 1;
-while (num <= 20) {
-  if (num % 3 == 0 && num % 5 == 0) {
+number = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+number.forEach(function(index){
+  if (index % 3 == 0 && index % 5 == 0) {
     console.log("FizzBuzz");
-  }else if(num % 3 == 0) {
+  }else if(index % 3 == 0) {
     console.log("Fizz");
-  }else if(num % 5 == 0) {
+  }else if(index % 5 == 0) {
     console.log("Buzz");
   }else {
-    console.log(num)
+    console.log(index)
   }
-  num += 1
-}
+});
