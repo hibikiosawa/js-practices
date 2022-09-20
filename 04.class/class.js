490dcee7d270f865acfa616d22bcc113e420cd96
osawa <hibiki2_627@yahoo.co.jp>
2022-09-20 16:54:11 +0900

    eslintの対応

--- 04.class/class.js
+++ 04.class/class.js
@@ -1,109 +1,103 @@
 const argv = require('minimist')(process.argv.slice(2))
-const fs = require("fs");
-const inquirer = require('inquirer');
-const readline = require('readline')
-const { TextDecoder } = require('util');
-
+const fs = require('fs')
+const inquirer = require('inquirer')
 
 class Memo {
   inputMemo () {
-    process.stdin.resume();
-    process.stdin.setEncoding('utf8');
-    let input_string = '';
+    process.stdin.resume()
+    process.stdin.setEncoding('utf8')
+    let inputString = ''
 
-    process.stdin.on('data', function(chunk) {
-      input_string += chunk;
-    });
+    process.stdin.on('data', function (chunk) {
+      inputString += chunk
+    })
 
-    process.stdin.on('end', function() {
-      const lines = input_string.split("\n");
+    process.stdin.on('end', function () {
+      const lines = inputString.split('\n')
 
-       fs.writeFile(`${lines[0]}.txt`, input_string, (err, data) => {
-        if(err) console.log(err);
-        else console.log('保存しました');
-      });
-    });
+      fs.writeFile(`${lines[0]}.txt`, inputString, (err, data) => {
+        if (err) console.log(err)
+        else console.log('保存しました')
+      })
+    })
   }
 
-  readFiles(files){
-        const fileList = files.filter(function(file){
-          return fs.statSync(file).isFile() && /.*\.txt$/.test(file); //絞り込み
-        })
-      return fileList
+  readFiles (files) {
+    const fileList = files.filter(function (file) {
+      return fs.statSync(file).isFile() && /.*\.txt$/.test(file) // 絞り込み
+    })
+    return fileList
   }
 
-  listMemo() {
-    fs.readdir('.', function(err, files){
-      if (err) throw err;
-      let fileList = memo.readFiles(files);
-      fileList.forEach(function(index){
-        const text = fs.readFileSync(index, 'utf8');
-          const lines = text.match(/^.*$/m);
-          console.log(lines[0]);
-      });
-  })
-}
+  listMemo () {
+    fs.readdir('.', function (err, files) {
+      if (err) throw err
+      const fileList = memo.readFiles(files)
+      fileList.forEach(function (index) {
+        const text = fs.readFileSync(index, 'utf8')
+        const lines = text.match(/^.*$/m)
+        console.log(lines[0])
+      })
+    })
+  }
 
-  referenceMemo() {
-    fs.readdir('.', function(err, files){
-      if (err) throw err;
-      let fileList = memo.readFiles(files);
+  referenceMemo () {
+    fs.readdir('.', function (err, files) {
+      if (err) throw err
+      const fileList = memo.readFiles(files)
       const fileText = []
-      fileList.forEach(function(index){
-        const text = fs.readFileSync(index, "utf8",function(err,result){
-            if(err) throw err;
-            console.log(data);
+      fileList.forEach(function (index) {
+        const text = fs.readFileSync(index, 'utf8', function (err, result) {
+          if (err) throw err
         })
-        const textSplited = text.split(/\r\n|\n/);
+        const textSplited = text.split(/\r\n|\n/)
         fileText.push(textSplited[0])
       })
-        const answer = inquirer.prompt([
-            {
-              message: 'ファイル詳細',
-              type: 'list',
-              name: 'title',
-              choices: fileText
-            }
-          ])
-          .then(answer => {
-            const text = fs.readFileSync(`${answer.title}.txt`, 'utf8');
-            console.log(text)
-          })
-      })
+      inquirer.prompt([
+        {
+          message: 'ファイル詳細',
+          type: 'list',
+          name: 'title',
+          choices: fileText
+        }
+      ])
+        .then(answer => {
+          const text = fs.readFileSync(`${answer.title}.txt`, 'utf8')
+          console.log(text)
+        })
+    })
   }
 
-  deleteMemo(){
-    fs.readdir('.', function(err, files){
-      if (err) throw err;
-      let fileList = memo.readFiles(files);
+  deleteMemo () {
+    fs.readdir('.', function (err, files) {
+      if (err) throw err
+      const fileList = memo.readFiles(files)
       const fileText = []
-      fileList.forEach(function(index){
-        const text = fs.readFileSync(index, "utf8",function(err,result){
-            if(err) throw err;
-            console.log(data);
+      fileList.forEach(function (index) {
+        const text = fs.readFileSync(index, 'utf8', function (err, result) {
+          if (err) throw err
         })
-        const textSplited = text.split(/\r\n|\n/);
+        const textSplited = text.split(/\r\n|\n/)
         fileText.push(textSplited[0])
       })
-      const answer = inquirer.prompt([
+      inquirer.prompt([
         {
           type: 'list',
           name: 'title',
           choices: fileText
         }
       ])
-      .then(answer => {
-        fs.unlink(`${answer.title}.txt`,(err) => {
-            if (err) throw err;
+        .then(answer => {
+          fs.unlink(`${answer.title}.txt`, (err) => {
+            if (err) throw err
             console.log('削除しました')
+          })
         })
     })
-    });
   }
-
 }
 
-const memo = new Memo
+const memo = new Memo()
 
 if (argv.l) {
   memo.listMemo()
